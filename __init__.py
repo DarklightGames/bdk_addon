f55d08876e4814027c069f2b7b098241c7e84dfc
Yurii Ti <mooncats@tutanota.com>
2023-01-13 17:10:09 +0200

    Incremental progress on static mesh copying
    - Added context menu entries for copying objects.
    - Added support for copying collection instances.
    - Added DrawScale3D to copy output.
    - Refactoring.

--- __init__.py
+++ __init__.py
@@ -23,6 +23,9 @@ if 'bpy' in locals():
     importlib.reload(terrain_operators)
 
     importlib.reload(bdk_panel)
+
+    importlib.reload(t3d_data)
+    importlib.reload(t3d_operators)
 else:
     from .material import data as material_data
     from .material import reader as material_reader
@@ -31,6 +34,10 @@ else:
     from .terrain import operators as terrain_operators
 
     from .panel import panel as bdk_panel
+
+    from .t3d import data as t3d_data
+    from .t3d import operators as t3d_operators
+
 
 import bpy
 
@@ -44,9 +51,11 @@ class BdkAddonPreferences(AddonPreferences):
         self.layout.prop(self, 'build_path')
 
 
-classes = material_importer.classes + terrain_operators.classes + bdk_panel.classes + (
-    BdkAddonPreferences,
-)
+classes = material_importer.classes + \
+          terrain_operators.classes + \
+          bdk_panel.classes + \
+          t3d_operators.classes + \
+          ( BdkAddonPreferences, )
 
 
 def material_import_menu_func(self, _context: bpy.types.Context):
@@ -58,13 +67,31 @@ def bdk_add_menu_func(self, _context: bpy.types.Context):
     self.layout.operator(terrain_operators.BDK_OT_TerrainInfoAdd.bl_idname, text='Terrain Info')
 
 
+def bdk_t3d_copy_func(self, _context: bpy.types.Context):
+    self.layout.separator()
+    self.layout.operator(t3d_operators.BDK_OP_CopyObject.bl_idname, icon='COPYDOWN')
+
+
+def bdk_t3d_copy_asset_func(self, _context: bpy.types.Context):
+    self.layout.separator()
+    self.layout.operator(t3d_operators.BDK_OP_CopyAsset.bl_idname, icon='COPYDOWN')
+
+
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
+
     bpy.types.TOPBAR_MT_file_import.append(material_import_menu_func)
 
     # For now, put the add-terrain operator in the add menu
     bpy.types.VIEW3D_MT_add.append(bdk_add_menu_func)
+
+    # T3D Copy (objects/collections)
+    bpy.types.VIEW3D_MT_object_context_menu.append(bdk_t3d_copy_func)
+    bpy.types.OUTLINER_MT_collection.append(bdk_t3d_copy_func)
+
+    # T3D Copy (assets)
+    bpy.types.ASSETBROWSER_MT_context_menu.append(bdk_t3d_copy_asset_func)
 
     bpy.types.Object.terrain_info = PointerProperty(type=terrain_operators.BDK_PG_TerrainInfoPropertyGroup)
 
@@ -72,7 +99,16 @@ def register():
 def unregister():
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
+
     bpy.types.TOPBAR_MT_file_import.remove(material_import_menu_func)
+    bpy.types.VIEW3D_MT_add.remove(bdk_add_menu_func)
+
+    # T3D Copy (objects/collections)
+    bpy.types.VIEW3D_MT_object_context_menu.remove(bdk_t3d_copy_func)
+    bpy.types.OUTLINER_MT_collection.remove(bdk_t3d_copy_func)
+
+    # T3D Copy (assets)
+    bpy.types.ASSETBROWSER_MT_context_menu.remove(bdk_t3d_copy_asset_func)
 
     del bpy.types.Object.terrain_info
 
--- panel/panel.py
+++ panel/panel.py
@@ -1,75 +1,5 @@
-import bpy
-from bpy.types import Panel, Operator, Object
-from dataclasses import dataclass
-from mathutils import Vector, Euler
-
-
-def rad_to_unreal(value: float) -> int:
-    return int(value * 10430.378350470452724949566316381)
-
-
-class URotator():
-    def __init__(self, euler: Euler):
-        self.pitch: int = -rad_to_unreal(euler.y)
-        self.roll: int = rad_to_unreal(euler.x)
-        self.yaw: int = -rad_to_unreal(euler.z)
-
-    def __str__(self) -> str:
-        return f'(Pitch={self.pitch},Roll={self.roll},Yaw={self.yaw})'
-
-
-class UVector():
-    def __init__(self, vector: Vector):
-        self.x: float = vector.x - 32.0
-        self.y: float = -vector.y - 32.0
-        self.z: float = vector.z - 32.0
-        
-
-    def __str__(self) -> str:
-        return f'(X={self.x},Y={self.y},Z={self.z})'
-
-
-class StaticMeshActor:
-    def __init__(self, object: Object, name: str) -> None:
-        loc = Vector(object.location)
-        rot = Euler(object.rotation_euler)
-
-        self.name = name
-
-        self.props = {}
-        self.props['StaticMesh'] = object.data.name
-        self.props['Location'] = str(UVector(loc))
-        self.props['Rotation'] = str(URotator(rot))
-
-    def __repr__(self) -> str:
-        out = f'Begin Actor Class=StaticMeshActor Name={self.name}'
-        indent = '    '
-
-        for _, (key, value) in enumerate(self.props.items()):
-            out += f'\n{indent}{key}={value}'
-
-        out += '\nEnd Actor'
-
-        return out
-
-
-class BDK_OP_CopyObject(Operator):
-    bl_idname = 'bdk_panel.copy_object'
-    bl_label = 'Copy Object(s)'
-    bl_options = {'INTERNAL', 'UNDO'}
-
-    def execute(self, context):
-        selected = bpy.context.selected_objects
-
-        pasta = 'Begin Map'
-        for idx, object in enumerate(selected):
-            pasta += '\n' + str(StaticMeshActor(object, f'StaticMeshActor{idx}'))
-        pasta += '\nBegin Surface\nEnd Surface\nEnd Map'
-
-        bpy.context.window_manager.clipboard = pasta
-        print(pasta)
-
-        return {'FINISHED'}
+from bpy.types import Panel
+from ..t3d.operators import BDK_OP_CopyObject
 
 
 class BDK_PT_Panel(Panel):
@@ -86,5 +16,4 @@ class BDK_PT_Panel(Panel):
 
 classes = (
     BDK_PT_Panel,
-    BDK_OP_CopyObject
 )
